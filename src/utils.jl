f97569ad6076ad93a5455698c1fba4f8b4beb782
JanJereczek <jan.jereczek@gmail.com>
2022-11-23 17:29:00 +0100

    type-stailized utils

--- src/utils.jl
+++ src/utils.jl
@@ -5,7 +5,7 @@
 Return a 2D meshgrid spanned by `x, y`.
 """
 function meshgrid(x::AbstractVector{T}, y::AbstractVector{T}) where {T<:AbstractFloat}
-    one_x, one_y = ones(length(x)), ones(length(y))
+    one_x, one_y = ones(T, length(x)), ones(T, length(y))
     return one_y * x', reverse((one_x * y')', dims=1)
 end
 
@@ -15,8 +15,8 @@ end
 
 Initialize a square computational domain with length `2*L` and `N^2` grid cells.
 """
-function init_domain(L::AbstractFloat, N::Int)
-    h = 2*L / N
+function init_domain(L::T, N::Int) where {T<:AbstractFloat}
+    h = T(2*L) / N
     x = collect(-L+h/2:h:L-h/2)
     X, Y = meshgrid(x, x)
     return DomainParams(L, N, h, x, X, Y)
