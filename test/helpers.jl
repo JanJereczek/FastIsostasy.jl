2a6068403507bfdcc4de16af6d1a6d472175809f
JanJereczek <jan.jereczek@gmail.com>
2022-12-01 15:24:53 +0100

    default n_quad_support

--- test/helpers.jl
+++ test/helpers.jl
@@ -1,5 +1,5 @@
 
-function mask_disc(X::Matrix{T}, Y::Matrix{T}, R::T) where {T<:AbstractFloat}
+function mask_disc(X::AbstractMatrix{T}, Y::AbstractMatrix{T}, R::T) where {T<:AbstractFloat}
     return T.(X .^ 2 + Y .^ 2 .< R^2)
 end
 
@@ -19,12 +19,13 @@ function analytic_solution(
     c::PhysicalConstants,
     p::SolidEarthParams,
     H0::T,
-    R0::T,
+    R0::T;
+    n_quad_support=10_000_000::Int,
 ) where {T<:AbstractFloat}
     scaling = c.rho_ice * c.g * H0 * R0
     integrand_rt(kappa) = analytic_integrand(kappa, r, t, c, p, R0)
     # lines(0:0.01:1, integrand_rt.(0f0:1f-2:1f0))
-    return scaling .* quadrature1D( integrand_rt, 10_000_000, T(0), T(1) )
+    return scaling .* quadrature1D( integrand_rt, n_quad_support, T(0), T(1) )
 end
 
 function analytic_integrand(
