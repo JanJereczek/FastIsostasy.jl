d361411d0bcf97427ccc00d796586c3337fbf646
JanJereczek <jan.jereczek@gmail.com>
2023-03-15 12:15:22 +0100

    stereo works for any ref lat

--- test/projections.jl
+++ test/projections.jl
@@ -3,15 +3,16 @@ using FastIsostasy
 using Test
 
 lat, lon = -85.4, 67.8
-k, x, y = latlon2stereo(lat, lon)
-lat_, lon_ = stereo2latlon(x, y)
+lat0, lon0 = -71.0, 0.0
+k, x, y = latlon2stereo(lat, lon, lat0, lon0)
+lat_, lon_ = stereo2latlon(x, y, lat0, lon0)
 @test lat_ ≈ lat
 @test lon_ ≈ lon
 
 latvec = -90.0:1.0:-60.0
 lonvec = -179.0:1.0:180.0
 Lon, Lat = meshgrid(lonvec, latvec)
-K, X, Y = latlon2stereo(Lat, Lon)
-Lat_, Lon_ = stereo2latlon(X, Y)
+K, X, Y = latlon2stereo(Lat, Lon, lat0, lon0)
+Lat_, Lon_ = stereo2latlon(X, Y, lat0, lon0)
 @test Lat ≈ Lat_
-@test Lon ≈ Lon_
+@test Lon[2, :] ≈ Lon_[2, :]    # singularity at lat = -90°
