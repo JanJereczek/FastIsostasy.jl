c4731c5daa9a9fec93650df70aff42a14838b932
JanJereczek <jan.jereczek@gmail.com>
2023-09-12 17:31:04 +0200

    use ICE6G since 122ka BP (test5c)

--- publication_v1.0/test5/test5c_compute.jl
+++ publication_v1.0/test5/test5c_compute.jl
@@ -1,40 +1,16 @@
 push!(LOAD_PATH, "../")
 using JLD2, NCDatasets, CairoMakie, Interpolations, DelimitedFiles
 
-datasets = ["ICE6G", "ICE7G"]
-prefixes = ["I6_C.VM5a_1deg.", "I7G_NA.VM7_1deg."]
-
-chooseidx = 1
-dataset = datasets[chooseidx]
-prefix = prefixes[chooseidx]
-
-dir = "../data/$dataset"
-files = readdir(dir)
-t = Float64[]
-for file in files
-    tstring = chop(file, head = length(prefix), tail = length(".nc"))
-    push!(t, parse(Float64, tstring))
-end
-idx = sortperm(-t)
-t = -t[idx]
-files = files[idx]
-nt = length(t)
-
-lon = -179:180
-lat = -89:90
-nlon, nlat = length(lon), length(lat)
-topodiff = zeros(nlon, nlat, nt)
-topo = zeros(nlon, nlat, nt)
-ice_thickness = zeros(nlon, nlat, nt)
-
-for k in eachindex(t)
-    NCDataset(joinpath(dir, files[k])) do ds
-        topodiff[:, :, k] = ds["Topo_Diff"][:, :]
-        topo[:, :, k] = ds["Topo"][:, :]
-        ice_thickness[:, :, k] = ds["stgit"][:, :]
-    end
-end
-Hsum = [sum(ice_thickness[:, :, k]) for k in eachindex(t)]
+file = "../data/ICE6Gzip/IceT.I6F_C.131QB_VM5a_1deg.nc"
+ds = NCDataset(file)
+Hice = copy(ds["stgit"][:, :])
+t = copy(ds["Time"][:, :])
+lat = copy(ds["Lat"][:, :])
+lon = copy(ds["Lon"][:, :]) .- 180
+close(ds)
+t .*= -1
+nlon, nlat, nt = size(Hice)
+Hsum = [sum(Hice[:, :, k]) for k in eachindex(t)]
 
 Hlim = (1e-8, 4e3)
 fig = Figure(resolution = (1600, 1000), fontsize = 24)
@@ -45,7 +21,7 @@ xlims!(ax2, extrema(t))
 ylims!(ax2, extrema(Hsum) .+ (-2e6, 2e6))
 
 kobs = Observable(1)
-Hobs = @lift( ice_thickness[:, :, $kobs] )
+Hobs = @lift( Hice[:, :, $kobs] )
 nobs = @lift( length(t[1:$kobs]) )
 tobs = @lift( t[1:$nobs] )
 Vproxy = @lift( Hsum[1:$nobs] )
@@ -53,16 +29,16 @@ heatmap!(ax1, Hobs, colormap = :ice, colorrange = Hlim, lowclip = :transparent)
 points = Observable(Point2f[(t[1], Hsum[1])])
 lines!(ax2, points)
 
-record(fig, "$dataset.mp4", eachindex(t), framerate = 10) do k
+record(fig, "ICE6G-cylce.mp4", eachindex(t), framerate = 10) do k
     kobs[] = k
     new_point = Point2f(t[k], Hsum[k])
     points[] = push!(points[], new_point)
 end
 
-Hitp = linear_interpolation((lon, lat, t), ice_thickness, extrapolation_bc = Flat())
+Hitp = linear_interpolation((lon, lat, t), Hice, extrapolation_bc = Flat())
 using FastIsostasy
 
-Omega = ComputationDomain(3000e3, 7)
+Omega = ComputationDomain(3500e3, 3500e3, 200, 200)
 c = PhysicalConstants()
 lbmantle = c.r_equator .- [5.721, 5.179] .* 1e6
 lb = vcat(96e3, lbmantle)
@@ -71,14 +47,15 @@ lv = [0.5, 1.58, 3.16] .* 1e21
 # lv = [5e20]
 p = LayeredEarth(Omega, layer_boundaries = lb, layer_viscosities = lv)
 
-Hice = [copy(Omega.null) for _ in t]
+Hice_vec = [copy(Omega.null) for _ in t]
 for k in eachindex(t)
     for IJ in CartesianIndices(Omega.Lat)
         i, j = Tuple(IJ)
-        Hice[k][i, j] = Hitp(Omega.Lon[i, j], Omega.Lat[i, j], t[k])
+        # println(Omega.Lon[i, j], "  ", Omega.Lat[i, j], "  ", t[k])
+        Hice_vec[k][i, j] = Hitp(Omega.Lon[i, j], Omega.Lat[i, j], t[k])
     end
 end
-deltaH = [Hice[k] - Hice[1] for k in eachindex(t)]
+deltaH = [Hice_vec[k] - Hice_vec[1] for k in eachindex(t)]
 
 tsec = years2seconds.(t .* 1e3)
 fip = FastIsoProblem(Omega, c, p, tsec, false, tsec, deltaH)
@@ -90,24 +67,44 @@ fig = Figure(resolution = (1600, 1000), fontsize = 30)
 axs = [Axis(fig[1, j], title = @lift("t = $(t[$kobs]) ka"),
     aspect = DataAspect()) for j in 1:2]
 [hidedecorations!(ax) for ax in axs]
-hm1 = heatmap!(axs[1], @lift(Hice[$kobs]), colorrange = Hlim, lowclip = :transparent, colormap = :ice)
-hm2 = heatmap!(axs[2], @lift(fip.out.u[$kobs]), colormap = :vik, colorrange = (-400, 400))
+hm1 = heatmap!(axs[1], @lift(Hice_vec[$kobs]), colorrange = Hlim, lowclip = :transparent, colormap = :ice)
+hm2 = heatmap!(axs[2], @lift(fip.out.u[$kobs]), colormap = :vik, colorrange = (-800, 800))
 Colorbar(fig[2, 1], hm1, vertical = false, flipaxis = false, width = Relative(0.8))
 Colorbar(fig[2, 2], hm2, vertical = false, flipaxis = false, width = Relative(0.8))
-record(fig, "$dataset-displacement.mp4", eachindex(t), framerate = 10) do k
+record(fig, "ICE6G-cycle-displacement.mp4", eachindex(t), framerate = 10) do k
+    kobs[] = k
+end
+
+dHlims = (-2600, 2600)
+ulims = (-500, 500)
+kobs = Observable(1)
+fig = Figure(resolution = (1600, 1000), fontsize = 30)
+vars = [deltaH, fip.out.u .+ fip.out.ue]
+axs = [Axis3(fig[1, j], title = @lift("t = $(t[$kobs]) ka,"*
+ "  range = $(round.(extrema(vars[j][$kobs])))")) for j in 1:2]
+# [hidedecorations!(ax) for ax in axs]
+sf1 = surface!(axs[1], @lift(deltaH[$kobs]), colorrange = dHlims, colormap = :vik)
+sf2 = surface!(axs[2], @lift(fip.out.u[$kobs] + fip.out.ue[$kobs]), colormap = :PuOr,
+    colorrange = ulims)
+zlims!(axs[1], dHlims)
+zlims!(axs[2], ulims)
+Colorbar(fig[2, 1], sf1, vertical = false, flipaxis = false, width = Relative(0.8))
+Colorbar(fig[2, 2], sf2, vertical = false, flipaxis = false, width = Relative(0.8))
+record(fig, "ICE6G-cycle-surface.mp4", eachindex(t), framerate = 10) do k
     kobs[] = k
 end
 
 
-#=
 latydir = "../data/Latychev/ICE6G/r1d_ICE6G"
 latyfiles = readdir(latydir)
 tlaty = [-120, -26, -21, -16, -11, -6, -2, -1, -0.125, 0, 0.125] .* 1e3
 
-latyfiles = latyfiles[2:end-1]
-tlaty = tlaty[2:end-1]
-=#
-
+latyfiles = latyfiles[1:end-1]
+tlaty = tlaty[1:end-1]
+for latyfile in latyfiles
+    displ = readdlm(joinpath(latydir, latyfile))
+    println(extrema(displ))
+end
 
 
 
