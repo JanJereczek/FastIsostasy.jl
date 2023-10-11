e773feedf541482ad1e6a12b48848a5b579e2a86
JanJereczek <jan.jereczek@gmail.com>
2023-10-11 18:04:13 +0200

    scaling of nfft kappa

--- src/nfft.jl
+++ src/nfft.jl
@@ -33,25 +33,29 @@ Omega = ComputationDomain(3000e3, 7)
 
 function get_kappa_dist(Omega, K)
     (;X, Y, Wx, Wy, Nx, Ny) = Omega
-    kappa = ifftshift(ifft(fftshift(Omega.pseudodiff)))
-    kkx = vec(K .* (X ./ (2 * Wx)) ./ maximum(K) )
-    kky = vec(K .* (Y ./ (2 * Wy)) ./ maximum(K) )
+    kappa_timedomain = ifftshift(ifft(fftshift(Omega.pseudodiff)))
+    kk = K ./ maximum(K)
+    kkx = vec(kk .* (X ./ (2 * Wx)) )
+    kky = vec(kk .* (Y ./ (2 * Wy)) )
     kmat = vcat(kkx', kky')
-    ynfft = nfft(kmat, kappa)
-    return reshape(ynfft, Nx, Ny)
+    kappa_nfft_vec = nfft(kmat, kappa_timedomain)
+    kappa_nfft = real.(reshape(kappa_nfft_vec, Nx, Ny))
+    return kappa_nfft
 end
 fourierderiv(Z, kappa) = real.(ifft(fft(Z) .* kappa))
 
 kappa0 = Omega.pseudodiff
 kappa1 = get_kappa_dist(Omega, ones(Omega.Nx, Omega.Ny))
 kappa2 = get_kappa_dist(Omega, Omega.K)
+kappa3 = kappa0 .* kappa1 ./ kappa2
 
 Z = 1e3 .* sin.(Omega.X ./ 0.5e6) .* sin.(Omega.Y ./ 0.5e6)
-A = 1 .+ # 1e-6 .* (Omega.Wx .- abs.(Omega.X)) .^ 2 + 1e-6 .* (Omega.Wy .- abs.(Omega.Y)) .^ 2 +
-    1e-11 .* (Omega.Wx .- abs.(Omega.X)) .* (Omega.Wy .- abs.(Omega.Y))
+A = 1 .+ 1e-11 .* (Omega.Wx .- abs.(Omega.X)) .* (Omega.Wy .- abs.(Omega.Y))
 Z0 = fourierderiv(Z, kappa0)
 Z1 = fourierderiv(Z, kappa1)
 Z2 = fourierderiv(Z, kappa2)
+Z3 = fourierderiv(Z, kappa3)
 A0 = fourierderiv(A, kappa0)
 A1 = fourierderiv(A, kappa1)
 A2 = fourierderiv(A, kappa2)
+A3 = fourierderiv(A, kappa3)
