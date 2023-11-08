510f2e4b49ac4002de4d7998f24f7f80ccc0c598
JanJereczek <jan.jereczek@gmail.com>
2023-11-08 15:19:41 +0100

    loader ice6g, wiens 2022, pan 2022, spada 2011

--- src/dataloaders.jl
+++ src/dataloaders.jl
@@ -1,281 +1,91 @@
+"""
+    load_dataset(name) → (dims), field, interpolator
 
-function load_dataset(name::String)
-    if name == "Etopo2022"
-        return load_bathymetry()
-    elseif name == "Spada2011"
-        return load_spada2011()
-    elseif name == "LatychevGaussian"
-        return load_latychev_gaussian(dir::String, x_lb::Real, x_ub::Real)
-    elseif name == "LatychevICE6G-1D"
-        return load_laty_ICE6G(case = "1D")
-    elseif name == "LatychevICE6G-3D"
-        return load_laty_ICE6G(case = "3D")
-    elseif name == "ICE6G_C"
-        return 
+Return the `dims::Tuple{Vararg{Vector}}`, the `field<:Array` and the `interpolator`
+corresponding to a data set defined by a unique `name::String`. For instance:
+
+```julia
+(lon180, lat, t), Hice, Hice_itp = load_dataset("ICE6G_D")
+```
+
+Following options are available for parameter fields:
+ - "ICE6G_D": ice loading history from ICE6G_D.
+ - "Wiens2022": viscosity field from (Wiens et al. 2022)
+ - "Lithothickness_Pan2022": lithospheric thickness field from (Pan et al. 2022)
+ - "Viscosity_Pan2022": viscosity field from (Pan et al. 2022)
+
+Following options are available for model results:
+ - "Spada2011"
+ - "LatychevGaussian"
+ - "LatychevICE6G"
+"""
+function load_dataset(name::String; kwargs...)
+    ########################## Param and forcing fields #######################
+    if name == "OceanSurfaceFunctionETOPO2022"
+        return load_oceansurfacefunction(; kwargs...)
     elseif name == "ICE6G_D"
-    elseif name == "Wiens_2022"
-
-    elseif name == "Ivins_2022"
-        return nothing
+        return load_ice6gd(; kwargs...)
+    elseif name == "Wiens2022"
+        return load_wiens2022(; kwargs...)
+    elseif name == "Lithothickness_Pan2022"
+        return load_lithothickness_pan2022(; kwargs...)
+    elseif name == "Viscosity_Pan2022"
+        return load_3Dvisc_pan2022(; kwargs...)
+    ########################## Model outputs ##################################
+    elseif name == "Spada2011"
+        return load_spada2011(; kwargs...)
+    # elseif name == "LatychevGaussian"
+    #     return load_latychev_gaussian(dir::String, x_lb::Real, x_ub::Real)
+    elseif name == "LatychevICE6G"
+        return load_laty_ICE6G(; kwargs...)
     end
 end
 
-"""
-    load_bathymetry()
-
-Load the bathymetry map from ETOPO-2022 on 1 minute arclength resolution.
-"""
-function load_bathymetry()
-    # https://www.ngdc.noaa.gov/thredds/catalog/global/ETOPO2022/60s/60s_bed_elev_netcdf/catalog.html?dataset=globalDatasetScan/ETOPO2022/60s/60s_bed_elev_netcdf/ETOPO_2022_v1_60s_N90W180_bed.nc
-    filename = joinpath(@__DIR__, "data/ETOPO_2022_v1_60s_N90W180_bed.nc")
-    ds = NCDataset(filename, "r")
-    lat = copy(ds["lat"][:,:])
-    lon = copy(ds["lon"][:,:])
-    bedrock_missing = copy(ds["z"][:,:])
-    close(ds)
-
-    bedrock = zeros(size(bedrock_missing))
-    bedrock .= bedrock_missing
-
-    return lon, lat, bedrock
+#############################################################
+# Parameter fields
+#############################################################
+function load_oceansurfacefunction()
+    link = "https://github.com/JanJereczek/IsostasyData/raw/main/ocean_surface/dz=0.1m.jld2"
+    tmp = Downloads.download(link, tempdir() *"/"* basename(link))
+    @load "$tmp" z_support A_support
+    return z_support, A_support, linear_interpolation(z_support, A_support)
 end
 
-
-function load_laty_3Dvisc()
-    file = "/home/jan/.julia/dev/FastIsostasy/data/Latychev/ICE6G/dense/stp_model_MH"
-    data = vec(readdlm(file, skipstart = 2))
-    r = data[1:64]
-    eta_ratio_vec = data[65:end]
-
-    lon = collect(range(0, stop = 360, length = 721))
-    lat = collect(range(90, stop = -90, length = 361))
-    nr, nlat, nlon = length(r), length(lat), length(lon)
-    eta_ratio = reshape(eta_ratio_vec, (nlon, nlat, nr))
-    heatmap(eta_ratio[:, :, end])
-    reverse!(eta_ratio, dims = 2)
-    reverse!(lat)
-    heatmap(eta_ratio[:, :, end])
-    eta1D = [3.16, 3.16, 1.58, 1.58, 0.5, 0.5] .* 1e21
-    r_eta1D = [3480000.0, 5173800.9, 5173800.95, 5701000.0, 5701000.05, 6371000.0]
-    eta1D_itp = linear_interpolation(r_eta1D, eta1D)
-    eta1D_3D = zeros((nlon, nlat, nr))
-    for i in axes(eta1D_3D, 1), j in axes(eta1D_3D, 2)
-        eta1D_3D[i, j, :] .= eta1D_itp.(r)
-    end
-    eta = eta1D_3D .* (10 .^ eta_ratio)
-    lon180, eta180 = lon360tolon180(lon, eta)
-    heatmap(eta180[:, :, end-5])
-    eta_itp = linear_interpolation((lon180, lat, r), eta180, extrapolation_bc = Flat())
-    return eta_itp
-end
-
-function load_ice6g()
-    file = "../data/ICE6Gzip/IceT.I6F_C.131QB_VM5a_1deg.nc"
-    ds = NCDataset(file)
-    Hice = copy(ds["stgit"][:, :])
-    t, lat, lon = copy(ds["Time"][:, :]), copy(ds["Lat"][:, :]), copy(ds["Lon"][:, :])
-    close(ds)
-    return Hice, t, lat, lon
-end
-
-function load_ice6gd(; key = "IceT")
-    file = "../data/ICE6G_D/ICE6GD_$key.nc"
-    ds = NCDataset(file)
-    t, lon, lat = copy(ds["Time"][:, :]), copy(ds["Lon"][:, :]), copy(ds["Lat"][:, :])
-    Hice = copy(ds["$key"])
+function load_ice6gd(; var = "IceT")
+    link = "https://github.com/JanJereczek/IsostasyData/raw/main/ice_history/"*
+        "ICE6G_D/ICE6GD_$var.nc"
+    tmp = Downloads.download(link, tempdir() *"/"* basename(link))
+    ds = NCDataset(tmp, "r")
+    t, lon, lat = copy(ds["Time"][:]), copy(ds["Lon"][:]), copy(ds["Lat"][:])
+    Hice = copy(ds["$var"][:, :, :])
     close(ds)
 
     t .*= -1
     lon180, Hice180 = lon360tolon180(lon, Hice)
     Hice_itp = linear_interpolation((lon180, lat, t), Hice180, extrapolation_bc = Flat())
 
-    return t, lat, lon, Hice, Hice_itp
+    println("returning: (lon180, lat, t), Hice, interpolator")
+    return (lon180, lat, t), Hice, Hice_itp
 end
 
-#############################################################
-# Model outputs
-#############################################################
-
-function load_spada2011()
-    prefix ="../testdata/Spada/"
-    cases = ["u_cap", "u_disc", "dudt_cap", "dudt_disc", "n_cap", "n_disc"]
-    snapshots = ["0", "1", "2", "5", "10", "inf"]
-    data = Dict{String, Vector{Matrix{Float64}}}()
-    for case in cases
-        tmp = Matrix{Float64}[]
-        for snapshot in snapshots
-            fname = string(prefix, case, "_", snapshot, ".csv")
-            append!(tmp, [readdlm(fname, ',', Float64)])
-        end
-        data[case] = tmp
-    end
-    return data
+function load_wiens2022(; extrapolation_bc = Throw())
+    link = "https://github.com/JanJereczek/IsostasyData/raw/main/parameter_fields/"*
+        "viscosity/wiens2022.nc"
+    tmp = Downloads.download(link, tempdir() *"/"* basename(link))
+    ds = NCDataset(tmp, "r")
+    x, y, z = copy(ds["x"][:]), copy(ds["y"][:]), copy(ds["z"][:])
+    log10visc = copy(ds["log10visc"][:, :, :])
+    close(ds)
+    println("returning: (x, y, z), log10visc, interpolator")
+    return (x, y, z), log10visc, linear_interpolation((x, y, z), log10visc,
+        extrapolation_bc = extrapolation_bc)
 end
 
-function load_latychev_gaussian(dir::String, x_lb::Real, x_ub::Real)
-    files = readdir(dir)
-    
-    x_full = readdlm(joinpath(dir, files[1]), ',')[:, 1]
-    idx = x_lb .< x_full .< x_ub
-    x = x_full[idx]
-
-    u = zeros(length(x), length(files))
-    for i in eachindex(files)
-        file = files[i]
-        # println( file, typeof( readdlm(joinpath(dir, file), ',')[:, 1] ) )
-        u[:, i] = readdlm(joinpath(dir, file), ',')[idx, 2]
-    end
-    # u .-= u[:, 1]
-
-    return x, u
-end
-
-function load_laty_ICE6G(; case = "1D")
-    if case == "1D_sparse"
-        latydir = "../data/Latychev/ICE6G/r1d_ICE6G"
-        tlaty = [-120, -26, -21, -16, -11, -6, -2, -1, -0.125, 0, 0.125] .* 1e3
-    elseif case == "1D"
-        latydir = "../data/Latychev/ICE6G/dense/1D/R"
-        tlaty = vec(readdlm("../data/Latychev/ICE6G/dense/tt_25.dat"))
-    elseif case == "3D"
-        latydir = "../data/Latychev/ICE6G/dense/3D/R"
-        tlaty = vec(readdlm("../data/Latychev/ICE6G/dense/tt_25.dat"))
-    end
-    latyfiles = readdir(latydir)
-
-    latyfiles = latyfiles[1:end-1]
-    tlaty = tlaty[1:end-1]
-    nlon, nlat = 512, 256
-    ulaty = zeros(nlon, nlat, length(tlaty))
-
-    for k in eachindex(latyfiles)
-        ulaty[:, :, k] = reshape(vec(readdlm(joinpath(latydir, latyfiles[k]))), nlon, nlat)
-    end
-
-    gllatlon_file = "../data/Latychev/ICE6G/gl256_LatLon"
-    LonLat, header = readdlm(gllatlon_file, header = true)
-    Lon = reshape(LonLat[:, 1], nlon, nlat)
-    Lat = reshape(LonLat[:, 2], nlon, nlat)
-    Lon, Lat = reverse(Lon, dims = 2), reverse(Lat, dims = 2)
-    ulaty = reverse(ulaty, dims = 2)
-
-    Lon, ulaty = lon360tolon180(Lon[:, 1], ulaty)
-
-    for k in eachindex(tlaty)
-        fig = Figure(resolution = (1600, 900), fontsize = 30)
-        ax = Axis(fig[1,1], aspect = DataAspect())
-        hidedecorations!(ax)
-        hm = heatmap!(ulaty[:, :, k], colorrange = (-400, 400), colormap = :PuOr)
-        Colorbar(fig[1, 2], hm, height = Relative(0.8), label = "Displacement (m)")
-        save("plots/test4/laty/ICE6G-$case-global-lorange-$(tlaty[k]).png", fig)
-    end
-    itp = linear_interpolation((Lon[:, 1], Lat[1, :], tlaty), ulaty,
-        extrapolation_bc = Flat())
-
-    return tlaty, ulaty, Lon, Lat, itp
-end
-
-#############################################################
-# Parameter fields
-#############################################################
-
-"""
-    load_wiens2021(Omega)
-
-Load the viscosity field from [whitehouse-solid-2019] on `Omega::ComputationDomain`
-and return:
- - `x` the vector of x coordinates,
- - `y` the vector of y coordinates,
- - `z` the vector of z coordinates,
- - `eta` the array containing the log10-viscosities,
- - `eta_itp` an interpolator of eta over x, y and z.
-"""
-function load_wiens2021(Omega::ComputationDomain)
-
-    X, Y, Nx, Ny = Omega.X, Omega.Y, Omega.Nx, Omega.Ny
-    dir = "../data/visc_field/Wiens2021"
-    jld2file = "Wiens2021-Nx$Nx-Ny$Ny.jld2"
-    jld2path = joinpath(dir, "../", jld2file)
-
-    if isfile(jld2path)
-        @load "$jld2path" x y z logvisc3D eta_itp
-    else
-        x, y = X[:, 1], Y[1, :]
-        z = [100e3, 200e3, 300e3]
-
-        # x (km), y (km), visc (log10 Pa s), depth (km), radius (km), lon (deg), lat (deg)
-        println("Loading raw data...")
-        rawdata = [readdlm(file) for file in readdir(dir, join = true)]
-        filtdata = [wiens_filter_nan_viscosity(M) for M in rawdata]
-        km2m!(filtdata)
-        M = [wiens_filter_xy_outsidedomain(m, x, y) for m in filtdata]
-        logvisc3D = zeros(Float64, (Nx, Ny, length(z)))
-        min_dist_idx = zeros(Int, (Nx, Ny))
-
-        samex = (M[1][:, 1] == M[2][:, 1] == M[3][:, 1])
-        samey = (M[1][:, 2] == M[2][:, 2] == M[3][:, 2])
-        if samex & samey
-            xw, yw = M[1][:, 1], M[1][:, 2]
-        else
-            error("Files don't have matching x,y over depth.")
-        end
-
-        println("Populating distance array...")
-        for idx in CartesianIndices(min_dist_idx)
-            i, j = Tuple(idx)
-            min_dist_idx[i, j] = argmin( (X[i,j] .- xw) .^ 2 + (Y[i,j] .- yw) .^ 2 )
-        end
-
-        println("Populating viscosity array...")
-        for idx in CartesianIndices(logvisc3D)
-            i, j, k = Tuple(idx)
-            logvisc3D[i, j, k] = M[k][min_dist_idx[i, j], 3]
-        end
-
-        println("Generating interpolator...")
-        eta_itp = linear_interpolation( (x,y,z), logvisc3D, extrapolation_bc = Flat() )
-        @save "$jld2path" x y z logvisc3D eta_itp
-
-    end
-    return (x, y, z), logvisc3D, eta_itp
-end
-
-function wiens_filter_nan_viscosity(M::Matrix{T}) where {T<:AbstractFloat}
-    return M[.!isnan.(M[:, 3]), :]
-end
-
-function wiens_filter_xy_outsidedomain(M::Matrix{T}, x, y) where {T<:AbstractFloat}
-    x1, x2 = extrema(x)
-    y1, y2 = extrema(y)
-    idx = (x1 .<= M[:, 1] .<= x2) .& (y1 .<= M[:, 2] .<= y2)
-    return M[idx, :]
-end
-
-function wiens_get_closest_eta(x::T, y::T, M::Matrix{T}) where {T<:AbstractFloat}
-    l = argmin( (x .- M[:, 1]) .^ 2 + (y .- M[:, 2]) .^ 2 )
-    return M[l, 3]
-end
-
-function km2m!(V::Vector{Matrix{T}}) where {T<:AbstractFloat}
-    for i in eachindex(V)
-        for j in [1, 2, 4, 5]
-            V[i][:, j] .*= T(1e3) 
-        end
-    end
-end
-
-"""
-    load_litho_thickness_laty(Omega)
-
-Load the lithospheric thickness on `Omega::ComputationDomain` and return:
- - `x` the vector of x coordinates,
- - `y` the vector of y coordinates,
- - `Tlitho` the array containing the lithospheric thickness,
- - `itp` an interpolator of `Tlitho` over `x, y`.
-"""
-function load_litho_thickness_laty()
-    file = "/home/jan/.julia/dev/FastIsostasy/data/Latychev/ICE6G/dense/LAB.llz"
-    data, head = readdlm(file, header = true)
+function load_lithothickness_pan2022()
+    link = "https://raw.githubusercontent.com/JanJereczek/IsostasyData/main/"*
+        "parameter_fields/lithothickness/pan2022.llz"
+    tmp = Downloads.download(link, tempdir() *"/"* basename(link))
+    data, head = readdlm(tmp, header = true)
     Lon_vec, Lat_vec, T_vec = data[:, 1], data[:, 2], data[:, 3]
     lon, lat = unique(Lon_vec), unique(Lat_vec)
     nlon, nlat = length(lon), length(lat)
@@ -284,5 +94,98 @@ function load_litho_thickness_laty()
     reverse!(lat)
     lon180, Tlitho180 = lon360tolon180(lon, Tlitho)
     itp = linear_interpolation((lon180, lat), Tlitho180[:, :, 1], extrapolation_bc = Flat())
+
+    println("returning: (lon180, lat), Tlitho, interpolator")
     return (lon180, lat), Tlitho, itp
 end
+
+function load_logvisc_pan2022()
+    link = "https://github.com/JanJereczek/IsostasyData/raw/main/parameter_fields/viscosity/pan2022.nc"
+    tmp = Downloads.download(link, tempdir() *"/"* basename(link))
+    ds = NCDataset(tmp, "r")
+    lon = copy(ds["lon"][:])
+    lat = copy(ds["lat"][:])
+    r = copy(ds["r"][:])
+    logvisc = copy(ds["eta"][:, :, :])
+    logvisc_itp = linear_interpolation((lon, lat, r), logvisc, extrapolation_bc = Flat())
+    println("returning: (lon180, lat, r), eta (in log10 space), interpolator")
+    return (lon, lat, r), logvisc, logvisc_itp
+end
+
+#############################################################
+# Model outputs
+#############################################################
+
+function spada_dims()
+    theta = 0:0.1:20
+    t = [0, 1, 2, 5, 10, 100] .* 1e3
+    return theta, t
+end
+
+spada_cases() = ["u_cap", "u_disc", "dudt_cap", "dudt_disc", "n_cap", "n_disc"]
+
+function load_spada2011()
+    theta = Dict{String, Vector{Float64}}()
+    t = Dict{String, Vector{Float64}}()
+    X = Dict{String, Matrix{Float64}}()
+    Xitp = Dict()
+    for case in spada_cases()
+        (i, j), k, l = load_spada2011(case)
+        theta[case] = i
+        t[case] = j
+        X[case] = k
+        Xitp[case] = l
+    end
+    println("returning dictionnaries (keys = spada_cases): (theta, t), X, interpolator")
+    return (theta, t), X, Xitp
+end
+
+function load_spada2011(case)
+    theta, t = spada_dims()
+    link = "https://github.com/JanJereczek/IsostasyData/raw/main/model_outputs/"*
+        "Spada-2011/$case.jld2"
+    tmp = Downloads.download(link, tempdir() *"/"* basename(link))
+    @load "$tmp" X
+    if occursin("n_", case)
+        reverse!(X, dims = 1)
+    end
+    return (theta, t), X, linear_interpolation((theta, t), X)
+end
+
+function load_latychev2023_ICE6G(; case = "1D", var = "R")
+
+    if case == "1D"
+        latydir = joinpath(@__DIR__, "../data/Latychev/ICE6G/dense/1D/$var")
+    elseif case == "3D"
+        latydir = joinpath(@__DIR__, "../data/Latychev/ICE6G/dense/3D/$var")
+    end
+    timestep_file = joinpath(@__DIR__, "../data/Latychev/ICE6G/dense/tt_25.dat")
+    tlaty = vec(readdlm(timestep_file))
+    latyfiles = readdir(latydir)
+
+    latyfiles = latyfiles[1:end-1]
+    tlaty = tlaty[1:end-1]
+    nlon, nlat = 512, 256
+    X = zeros(nlon, nlat, length(tlaty))
+
+    for k in eachindex(latyfiles)
+        X[:, :, k] = reshape(vec(readdlm(joinpath(latydir, latyfiles[k]))),
+            nlon, nlat)
+    end
+
+    gllatlon_file = "../data/Latychev/ICE6G/gl256_LatLon"
+    LonLat, header = readdlm(gllatlon_file, header = true)
+    Lon = reshape(LonLat[:, 1], nlon, nlat)
+    Lat = reshape(LonLat[:, 2], nlon, nlat)
+    Lon, Lat = reverse(Lon, dims = 2), reverse(Lat, dims = 2)
+    X = reverse(X, dims = 2)
+
+    Lon, X = lon360tolon180(Lon[:, 1], X)
+    lon, lat = Lon[:, 1], Lat[1, :]
+    itp = linear_interpolation((lon, lat, tlaty), X, extrapolation_bc = Flat())
+
+    println("returning (var determined by kwarg): (lon, lat, t), var, interpolator")
+    return (lon, lat, tlaty), X, itp
+end
+
+
