1a3006b31fbb145bf6682b2281e0ac37c4946677
JanJereczek <jan.jereczek@gmail.com>
2023-11-15 14:56:05 +0100

    add residual field to osc

--- src/adaptive_ocean.jl
+++ src/adaptive_ocean.jl
@@ -8,7 +8,7 @@ Return a `mutable struct OceanSurfaceChange` containing:
  - `z_k`: the GMSL at time step `k`.
 
 An `osc::OceanSurfaceChange` can be used as function to update `osc.z_k` and `osc.A_k`
-based on `osc.A_itp` and an input `delta_V::Float64` by running:
+based on `osc.A_itp` and an input `delta_V` by running:
 
 ```julia
 osc(delta_V)
@@ -17,16 +17,18 @@ osc(delta_V)
 mutable struct OceanSurfaceChange{T<:AbstractFloat}
     z_k::T                  # sealevel at time step k
     A_k::T                  # ocean surface at time step k
-    z::AbstractVector{T}    # support for pwlinear interpolator
-    A::AbstractVector{T}    # values for pwlinear interpolator
-    A_itp                   # pwlinear interpolator A(z) ::Interpolations.Extrapolation
+    z::Vector{T}            # support for pwlinear interpolator
+    A::Vector{T}            # values for pwlinear interpolator
+    A_itp::Any
     A_pd::T                 # present-day ocean surface
+    residual::T
 end
-
-function OceanSurfaceChange(; A_ocean_pd::T = 3.625e14, z0::T = 0.0) where {T<:AbstractFloat}
+# ::{T, 1, Interpolations.Extrapolation{T, 1, Interpolations.GriddedInterpolation{T, 1, Vector{T},
+# Gridded{Linear{Throw{OnGrid}}}, Tuple{Vector{T}}}, Gridded{Linear{Throw{OnGrid}}}, Throw{Nothing}}}
+function OceanSurfaceChange(; T = Float64, A_ocean_pd = T(3.625e14), z0 = T(0.0))
     z, A, itp = load_oceansurfacefunction()
-    A_itp(z) = A_ocean_pd / itp(0.0) * itp(z)
-    return OceanSurfaceChange(z0, A_itp(z0), z, A, A_itp, A_ocean_pd)
+    A_itp(z) = A_ocean_pd / itp(T(0.0)) * itp(z)
+    return OceanSurfaceChange(z0, A_itp(z0), z, A, A_itp, A_ocean_pd, T(1e10))
 end
 
 function (osc::OceanSurfaceChange{T})(delta_V::T) where {T<:AbstractFloat}
@@ -44,11 +46,10 @@ function (osc::OceanSurfaceChange{T})(delta_V::T) where {T<:AbstractFloat}
             sol = mcpsolve(scr!, [minimum(osc.z)], [osc.z_k], [osc.z_k]; mcp_opts...)
         end
 
-        residual = (sol.zero[1] - osc.z_k) * mean([osc.A_itp(sol.zero[1]),
-            osc.A_itp(osc.z_k)]) - delta_V
+        surfacechange_residual!(osc, sol.zero[1], delta_V)
 
         # if sol.f_converged || sol.x_converged
-        if residual < 1e-5 * osc.A_pd   # residual must be less than 10 μm sea level
+        if osc.residual < 1e-5 * osc.A_pd   # residual must be less than 10 μm sea level
             osc.z_k = sol.zero[1]
             osc.A_k = osc.A_itp(osc.z_k)
         else
@@ -61,8 +62,7 @@ end
 
 
 """
-    surfacechange_residual!(Vresidual::Vector, z::Vector, z_k::T,
-        A_itp::Interpolations.Extrapolation, delta_V::T) where {T<:Float64}
+    surfacechange_residual!(Vresidual, z, z_k, A_itp, delta_V)
 
 Return `A_support::Vector{T}`, the ocean surface evaluated at `z_support` based
 on `bedrock` topography and distortion factor embedded in `cellsurface`.
@@ -70,6 +70,12 @@ The two former ones are then used as support points to construct an interpolator
 of ocean surface over depth.
 """
 function surfacechange_residual!(Vresidual::Vector, z::Vector, z_k::T,
-    A_itp, delta_V::T) where {T<:Float64}
+    A_itp, delta_V::T) where {T<:AbstractFloat}
     Vresidual[1] = (z[1] - z_k) * mean([A_itp(z[1]), A_itp(z_k)]) - delta_V
+end
+
+function surfacechange_residual!(osc::OceanSurfaceChange{T}, z::T, delta_V::T) where
+    {T<:AbstractFloat}
+    osc.residual = (z - osc.z_k) * mean([osc.A_itp(z), osc.A_itp(osc.z_k)]) - delta_V
+    return
 end
