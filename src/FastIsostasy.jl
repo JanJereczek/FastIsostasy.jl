ce34b882d14a4cb20ab6a7f508d1881e2a1b9b9d
JanJereczek <jan.jereczek@gmail.com>
2024-04-25 15:53:34 +0200

    explicit imports

--- src/FastIsostasy.jl
+++ src/FastIsostasy.jl
@@ -1,37 +1,41 @@
 module FastIsostasy
 
-using LinearAlgebra
-using Statistics: mean, cov
-using Distributions: MvNormal
-using .Threads
-using Downloads
-
-using JLD2
-using NCDatasets
-using DelimitedFiles: readdlm
-
-using Interpolations: linear_interpolation, Flat
-using NLsolve: mcpsolve
-using FFTW: fft, ifft, plan_fft!, plan_ifft!, cFFTWPlan
-using AbstractFFTs
-using FastGaussQuadrature: gausslegendre
+using AbstractFFTs: AbstractFFTs
 using CUDA: CuArray, CuMatrix, CUFFT, allowscalar
-using OrdinaryDiffEq: ODEProblem, solve, remake, OrdinaryDiffEqAlgorithm
-using EnsembleKalmanProcesses
-using EnsembleKalmanProcesses.Observations
-using EnsembleKalmanProcesses.ParameterDistributions
-using SpecialFunctions: besselj0, besselj1
+using DelimitedFiles: readdlm
+using Distributions: MvNormal
+using Downloads: download
 using DynamicalSystemsBase: CoupledODEs, trajectory
-using ParallelStencil
+using EnsembleKalmanProcesses: EnsembleKalmanProcesses, EnsembleKalmanProcess,
+                               Unscented, get_error, get_g_mean_final,
+                               get_ϕ_final, get_ϕ_mean_final
+using EnsembleKalmanProcesses.Observations: Observations
+using EnsembleKalmanProcesses.ParameterDistributions: ParameterDistributions,
+                                                      ParameterDistribution,
+                                                      combine_distributions,
+                                                      constrained_gaussian
+using FastGaussQuadrature: gausslegendre
+using FFTW: fft, ifft, plan_fft!, plan_ifft!, cFFTWPlan
+using Interpolations: Flat, Gridded, Linear, OnGrid, Throw, linear_interpolation
+using JLD2: @load, jldopen, load
+using LinearAlgebra: Diagonal, det, diagm, norm
+using NCDatasets: NCDatasets, NCDataset, defDim, defVar
+using NLsolve: mcpsolve
+using OrdinaryDiffEq: ODEProblem, solve, remake, OrdinaryDiffEqAlgorithm
+using ParallelStencil: ParallelStencil, @init_parallel_stencil, @parallel,
+                       @parallel_indices
+using Statistics: mean, cov
+using SpecialFunctions: besselj0, besselj1
+using .Threads
 
-using Reexport
+using Reexport: Reexport, @reexport
 @reexport using Interpolations
 @reexport using OrdinaryDiffEq: Euler, Midpoint, Heun, Ralston, BS3, BS5, RK4,
     OwrenZen3, OwrenZen4, OwrenZen5, Tsit5, DP5, RKO65, TanYam7, DP8,
-    Feagin10, Feagin12, Feagin14, TsitPap8, Vern6, Vern7, Vern8, Vern9,
-    SSPRK22, SSPRK33, SSPRK53, SSPRK63, SSPRK73, SSPRK83, SSPRK432, SSPRK43,
-    SSPRK932, SSPRK54, SSPRK104, SSPRKMSVS32, SSPRKMSVS43, SSPRK53_2N1, SSPRK53_2N2,
-    KenCarp47
+    Feagin10, Feagin12, Feagin14, TsitPap8, Vern6, Vern7, Vern8, Vern9
+    # SSPRK22, SSPRK33, SSPRK53, SSPRK63, SSPRK73, SSPRK83, SSPRK432, SSPRK43,
+    # SSPRK932, SSPRK54, SSPRK104, SSPRKMSVS32, SSPRKMSVS43, SSPRK53_2N1, SSPRK53_2N2,
+    # KenCarp47
 
 include("convenience_types.jl")
 include("convolution.jl")
@@ -94,10 +98,12 @@ export analytic_solution
 
 # data loaders
 export load_dataset, get_greenintegrand_coeffs
-export load_etopo2022, bathymetry, load_wiens2022
+export load_etopo2022, load_wiens2022
 export load_lithothickness_pan2022, load_logvisc_pan2022
 export load_ice6gd
 export load_spada2011, spada_cases
 export load_latychev_test3, load_latychev2023_ICE6G
+
+export update_second_derivatives!
 
 end
