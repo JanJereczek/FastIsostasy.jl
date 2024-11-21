db6eff8e899277fbdfd9388f332eedd4b0e8f082
JanJereczek <jan.jereczek@gmail.com>
2024-11-21 09:38:29 +0100

    rm DynamicalSystemsBase

--- src/FastIsostasy.jl
+++ src/FastIsostasy.jl
@@ -4,14 +4,12 @@ using AbstractFFTs: AbstractFFTs
 using CUDA: CuArray, CuMatrix, CUFFT, allowscalar
 using DelimitedFiles: readdlm
 using Downloads: download
-using DynamicalSystemsBase: CoupledODEs, trajectory
 using FastGaussQuadrature: gausslegendre
 using FFTW: fft, ifft, plan_fft!, plan_ifft!, cFFTWPlan
-using Interpolations: Flat, Gridded, Linear, OnGrid, Throw, linear_interpolation
 using LinearAlgebra: Diagonal, det, diagm, norm
 using NetCDF
 using NLsolve: mcpsolve
-using OrdinaryDiffEqTsit5: Tsit5, ODEProblem, solve, remake
+using OrdinaryDiffEqTsit5: init, Tsit5, ODEProblem, solve, DiscreteCallback
 using ParallelStencil: ParallelStencil, @init_parallel_stencil, @parallel,
                        @parallel_indices
 using Statistics: mean, cov, std
@@ -24,16 +22,18 @@ allowscalar(false)
 using Reexport: Reexport, @reexport
 @reexport using Interpolations
 @reexport using Proj
+@reexport using OrdinaryDiffEqTsit5: step!
 
 include("convenience_types.jl")
 include("convolution.jl")
 include("adaptive_ocean.jl")
+include("layering.jl")
 include("structs.jl")
 include("utils.jl")
 include("derivatives.jl")
 include("derivatives_parallel.jl")
-include("integrators.jl")
 include("geostate.jl")
+include("material.jl")
 include("mechanics.jl")
 include("analytic_solutions.jl")
 include("dataloaders.jl")
@@ -47,37 +47,41 @@ export InplaceConvolution
 # structs.jl
 export ComputationDomain, PhysicalConstants
 export ReferenceEarthModel, LayeredEarth, SolverOptions
-export CurrentState, ReferenceState
+export CurrentState, ReferenceState, NetcdfOutput
 export FastIsoTools, FastIsoProblem
-export NetcdfOutput, write_step!
 
 # utils.jl
 export years2seconds, seconds2years, m_per_sec2mm_per_yr
 export lon360tolon180
 export reinit_structs_cpu, meshgrid, kernelcollect
 
-export get_r, gauss_distr, generate_gaussian_field, samesize_conv, blur
+export get_quad_coeffs, get_r, gauss_distr, generate_gaussian_field, samesize_conv, blur
 export uniform_ice_cylinder, stereo_ice_cylinder, stereo_ice_cap
-export write_out!, remake!, savefip, null, not, cudainfo
+export write_nc!, write_out!, remake!, null, not, cudainfo
 
 # adaptive_ocean.jl
 export OceanSurfaceChange
 
 # geostate.jl
-export update_loadcolumns!, update_elasticresponse!, update_dz_ss!
+export update_loadcolumns!, update_elasticresponse!, update_dz_ss!, get_dz_ssgreen
 export columnanom_load!, columnanom_full!, columnanom_ice, columnanom_water
 export columnanom_litho!, columnanom_mantle!, update_z_ss!, total_volume
 export update_V_af!, update_V_den!, update_V_pov!, height_above_floatation
 
-# mechanics.jl
-export init, solve!, step!, lv_elva!, update_diagnostics!
-export maxwelltime_scaling!, compute_shearmodulus, get_rigidity, load_prem
-export get_flexural_lengthscale, get_kei, calc_kei_value, calc_viscous_green
-export update_deformation_rhs!
-export build_greenintegrand, get_quad_coeffs, get_elasticgreen, get_dz_ssgreen
+# layering.jl
+export AbstractLayering, UniformLayering, ParallelLayering, EqualizedLayering
+export FoldedLayering, get_layer_boundaries
 
-# integrators.jl
-export simple_euler!, SimpleEuler
+# material.jl
+export maxwelltime_scaling!, get_shearmodulus, get_rigidity, load_prem
+
+# mechanics.jl
+export init_integrator, solve!, elra!, lv_elva!, update_diagnostics!  # step!
+export update_deformation_rhs!, build_greenintegrand, get_elasticgreen
+export get_horizontal_displacement
+
+# elra.jl
+export get_flexural_lengthscale, get_kei, calc_kei_value, calc_viscous_green
 
 # analytic solutions
 export analytic_solution
