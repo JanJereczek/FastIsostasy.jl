644fb4a7ce5280c36aa6ea6e6766a033e35d1948
JanJereczek <jan.jereczek@gmail.com>
2024-11-27 13:34:17 +0100

    cases seperated from publication

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,7 +10,7 @@ using Test
 include("helpers/benchmark_constants.jl")
 include("helpers/compute.jl")
 include("helpers/plot.jl")
-include("../publication_v1.0/test3/cases.jl")
+include("helpers/cases.jl")
 include("../publication_v1.0/helpers_computation.jl")
 
 const SAVE_PLOTS = true
@@ -27,6 +27,7 @@ include("test_benchmarks.jl")
     benchmark1()
     benchmark1_float32()
     benchmark1_external_loadupdate()
+    # benchmark1_gpu()
     benchmark2()
     benchmark3()
     # benchmark5()
