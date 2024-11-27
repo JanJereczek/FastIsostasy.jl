2aef56735a6fc8262fedb9073b21e8ef3159f651
JanJereczek <jan.jereczek@gmail.com>
2024-11-27 13:34:00 +0100

    go back to old cases

--- test/helpers/cases.jl
+++ test/helpers/cases.jl
@@ -1,5 +1,3 @@
-using LinearAlgebra
-
 function choose_case(case::String, Omega::ComputationDomain)
 
     eta = 1e21
@@ -7,22 +5,19 @@ function choose_case(case::String, Omega::ComputationDomain)
     sigma = diagm([(W/4)^2, (W/4)^2])
 
     if case == "homogeneous"
-        lv = fill(eta, Omega.Nx, Omega.Ny)
-        litho_thickness = 150e3
-        layering = UniformLayering(1, [litho_thickness])
+        lb1 = fill(150e3, Omega.Nx, Omega.Ny)
+        lb = cat(lb1, dims=3)
+        lv = fill(eta, size(lb))
     elseif case == "gaussian_lo_D" || case == "gaussian_hi_D" || case == "no_litho"
         if case == "gaussian_lo_D"
-            litho_thickness = generate_gaussian_field(Omega, 150e3, [0.0, 0.0], -100e3, sigma)
-            lb2 = 151e3
-            layering = EqualizedLayering(2, [88e3, lb2])
+            lb1 = generate_gaussian_field(Omega, 150e3, [0.0, 0.0], -100e3, sigma)
+            lb2 = fill(151e3, Omega.Nx, Omega.Ny)
         elseif case == "gaussian_hi_D"
-            litho_thickness = generate_gaussian_field(Omega, 150e3, [0.0, 0.0], 100e3, sigma)
-            lb2 = 251e3
-            layering = EqualizedLayering(2, [88e3, lb2])
+            lb1 = generate_gaussian_field(Omega, 150e3, [0.0, 0.0], 100e3, sigma)
+            lb2 = fill(251e3, Omega.Nx, Omega.Ny)
         elseif case == "no_litho"
-            litho_thickness = 0.001e3
-            lb2 = 151e3
-            layering = EqualizedLayering(2, [litho_thickness, lb2])
+            lb1 = fill(0.001e3, Omega.Nx, Omega.Ny)
+            lb2 = fill(151e3, Omega.Nx, Omega.Ny)
         end
         lb = cat(lb1, lb2, dims=3)
         lv = fill(eta, size(lb))
@@ -45,8 +40,7 @@ function choose_case(case::String, Omega::ComputationDomain)
         lb = cat(lb1, lb2, dims=3)
         lv = cat(lv1, lv2, dims=3)
     end
-    p = LayeredEarth(Omega, U, layering = EqualizedLayering(2, [88e3, lb2]), layer_viscosities = lv,
-        rho_uppermantle = 3.6e3, litho_thickness = lb1)
+    p = LayeredEarth(Omega, layer_boundaries = lb, layer_viscosities = lv, rho_uppermantle = 3.6e3)
 
     return p, lb, lv
 end
