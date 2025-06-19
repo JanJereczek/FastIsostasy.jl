3fbe0081fd9831b78f5f034f054da01b4912d069
JanJereczek <jan.jereczek@gmail.com>
2025-06-19 12:04:57 +0200

    add docstrings

--- src/boundary_conditions.jl
+++ src/boundary_conditions.jl
@@ -1,15 +1,53 @@
+"""
+    AbstractBC
+
+An abstract type representing a boundary condition in the context of a computational domain.
+Available subtypes are:
+- [`OffsetBC`](@ref)
+"""
 abstract type AbstractBC end
 
-abstract type BCSpace end
-struct RegularBCSpace <: BCSpace end
-struct ExtendedBCSpace <: BCSpace end
+"""
+    AbstractBCSpace
+
+An abstract type representing the space in which boundary conditions are defined.
+This typically needs to be defined when initializing an [`AbstractBCSpace`](@ref).
+Available subtypes are:
+- [`RegularBCSpace`](@ref)
+- [`ExtendedBCSpace`](@ref)
+"""
+abstract type AbstractBCSpace end
+
+"""
+    RegularBCSpace <: AbstractBCSpace
+
+Singleton struct to impose boundary conditions at the edges of the computation domain.
+"""
+struct RegularBCSpace <: AbstractBCSpace end
+
+"""
+    ExtendedBCSpace <: AbstractBCSpace
+
+Singleton struct to impose boundary conditions at the edges of the extended
+computation domain, which naturally arises from convolutions.
+"""
+struct ExtendedBCSpace <: AbstractBCSpace end
 
 #########################################################################
 # Computation-level
 #########################################################################
 
+"""
+    OffsetBC{T} <: AbstractBC
+
+A boundary condition that applies an offset to the values at the boundaries of a
+computational domain. Contains:
+- `space`: the [`AbstractBCSpace`](@ref) in which the boundary condition is defined.
+- `x_border`: the offset value to be applied at the boundaries.
+- `W`: a weight matrix to apply the boundary condition according to some [ÀbstractBCRule](@ref).
+"""
 struct OffsetBC{T} <: AbstractBC
-    space::BCSpace
+    space::AbstractBCSpace
     x_border::T
     W::KernelMatrix{T}
 end
@@ -28,23 +66,44 @@ end
 # API level
 #########################################################################
 
+"""
+    CornerBC
+
+Impose a Dirichlet-like boundary condition at the corners of the computational domain.
+"""
 struct CornerBC
-    space::BCSpace
+    space::AbstractBCSpace
     x_border
 end
 
+"""
+    BorderBC
+
+Impose a Dirichlet-like boundary condition at the borders of the computational domain.
+"""
 struct BorderBC
-    space::BCSpace
+    space::AbstractBCSpace
     x_border
 end
 
+"""
+    DistanceWeightedBC
+
+Impose a Dirichlet-like boundary condition at the borders of the computational domain,
+weighted by the distance from the center of the domain.
+"""
 struct DistanceWeightedBC
-    space::BCSpace
+    space::AbstractBCSpace
     x_border
 end
 
+"""
+    MeanBC
+
+Impose a mean value for the field.
+"""
 struct MeanBC
-    space::BCSpace
+    space::AbstractBCSpace
     x_border
 end
 
@@ -76,7 +135,7 @@ function norm!(W)
 end
 
 """
-    precompute_bc(bc::AbstractBC, sp::BCSpace, Omega::ComputationDomain)
+    precompute_bc(bc::AbstractBC, sp::AbstractBCSpace, Omega::ComputationDomain)
 
 Precompute the boundary condition for the given computation domain.
 """
