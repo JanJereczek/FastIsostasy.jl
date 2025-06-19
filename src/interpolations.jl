c215636793c49ef3aa75502f0431ef5402a3b63b
JanJereczek <jan.jereczek@gmail.com>
2025-06-19 12:08:36 +0200

    Interpolations 0D

--- src/interpolations.jl
+++ src/interpolations.jl
@@ -1,6 +1,37 @@
-struct TimeInterpolation0D{T<:AbstractFloat}
-    t::Vector{T}
-    X::Vector{T}
+struct Interpolation0D{T<:AbstractFloat}
+    x::Vector{T}
+    y::Vector{T}
+    flat_bc::Bool
+end
+
+function Interpolation0D(x, y; flat_bc = false)
+    if length(x) != length(y)
+        throw(ErrorException("Interpolation0D: t and x must have the same length."))
+    end
+    return Interpolation0D(x, y, flat_bc)
+end
+
+function (itp::Interpolation0D)(x_out)
+    if x_out < minimum(itp.x)
+        if itp.flat_bc
+            return itp.y[1]
+        else
+            throw(ErrorException("Interpolation0D out of range."))
+        end
+    elseif x_out > maximum(itp.x)
+        if itp.flat_bc
+            return itp.y[end]
+        else
+            throw(ErrorException("Interpolation0D out of range."))
+        end
+    elseif x_out in itp.x
+        i = searchsortedfirst(itp.x, x_out)
+        return itp.y[i]
+    else
+        i = searchsortedfirst(itp.x, x_out) - 1
+        return itp.y[i] + (itp.y[i+1] - itp.y[i]) /
+            (itp.x[i+1] - itp.x[i]) * (x_out - itp.x[i])
+    end
 end
 
 mutable struct TimeInterpolation2D{T<:AbstractFloat, M<:KernelMatrix{T}}
@@ -11,28 +42,11 @@ mutable struct TimeInterpolation2D{T<:AbstractFloat, M<:KernelMatrix{T}}
     flat_bc::Bool
 end
 
-# mutable struct TimeInterpolation3D{T<:AbstractFloat, M<:KernelMatrix{T}}
-#     t::Vector{T}
-#     X::AbstractArray{T, 3}
-#     tdiff::Vector{T}
-#     i::Int
-# end
-
-# struct TimeInterpolation3D
-
 function TimeInterpolation2D(t, X; flat_bc = false)
     tdiff = similar(t)
     i = 0
     return TimeInterpolation2D(t, X, tdiff, i, flat_bc)
 end
-
-# function piecewise_constant_interpolate!(X_out::M, t::T, ti::TimeInterpolation2D{T, M}) where
-#     {T<:AbstractFloat, M<:KernelMatrix{T}}
-#     ti.tdiff .= ti.t .- t
-#     ti.i = argmin(abs.(ti.tdiff))
-#     X_out .= ti.X[ti.i]
-#     return nothing
-# end
 
 function piecewise_linear_interpolate!(X_out::M, t::T, ti::TimeInterpolation2D{T, M}) where
     {T<:AbstractFloat, M<:KernelMatrix{T}}
