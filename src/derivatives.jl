1a1d6db218618619aab6b52c2ed6c9aa679577cf
JanJereczek <jan.jereczek@gmail.com>
2025-06-26 17:15:24 +0200

    add FiniteDifferences to optionally use higher order schemes

--- src/derivatives.jl
+++ src/derivatives.jl
@@ -1,22 +1,69 @@
+using FiniteDifferences
+# using StaticArraysCore
+
+struct FiniteDiffMethod{T}
+    grid::Vector{Int}
+    coefs::Vector{T}
+end
+
+struct FiniteDiffParams{T}
+    order::Int
+    r::Int
+    d1_central::FiniteDiffMethod{T}
+    d1_forward::FiniteDiffMethod{T}
+    d1_backward::FiniteDiffMethod{T}
+    d2_central::FiniteDiffMethod{T}
+    d2_forward::FiniteDiffMethod{T}
+    d2_backward::FiniteDiffMethod{T}
+end
+
+function FiniteDiffParams(; order = 5, T = Float32)
+    r = order ÷ 2
+    d1_central = FiniteDifferences.central_fdm(order, 1)
+    d1_forward = FiniteDifferences.forward_fdm(order, 1)
+    d1_backward = FiniteDifferences.backward_fdm(order, 1)
+    d2_central = FiniteDifferences.central_fdm(order, 2)
+    d2_forward = FiniteDifferences.forward_fdm(order, 2)
+    d2_backward = FiniteDifferences.backward_fdm(order, 2)
+
+    d1_central = FiniteDiffMethod(Vector(d1_central.grid), Vector(T.(d1_central.coefs)))
+    d1_forward = FiniteDiffMethod(Vector(d1_forward.grid), Vector(T.(d1_forward.coefs)))
+    d1_backward = FiniteDiffMethod(Vector(d1_backward.grid), Vector(T.(d1_backward.coefs)))
+    d2_central = FiniteDiffMethod(Vector(d2_central.grid), Vector(T.(d2_central.coefs)))
+    d2_forward = FiniteDiffMethod(Vector(d2_forward.grid), Vector(T.(d2_forward.coefs)))
+    d2_backward = FiniteDiffMethod(Vector(d2_backward.grid), Vector(T.(d2_backward.coefs)))
+
+    return FiniteDiffParams(
+        order,
+        r,
+        d1_central,
+        d1_forward,
+        d1_backward,
+        d2_central,
+        d2_forward,
+        d2_backward
+    )
+end
+
 function update_second_derivatives!(uxx, uyy, ux, uxy, u, Omega)
     update_second_derivatives!(uxx, uyy, ux, uxy, u, u, u, Omega)
 end
 
 function update_second_derivatives!(uxx::M, uyy::M, ux::M, uxy::M, u1::M, u2::M, u3::M,
-    Omega::ComputationDomain{T, L, M}) where {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
+    Omega::RegionalComputationDomain{T, L, M}) where {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     dxx!(uxx, u1, Omega)
     dyy!(uyy, u2, Omega)
     dxy!(ux, uxy, u3, Omega)
 end
 
-function dxx(u::M, Omega::ComputationDomain{T, L, M}) where
+function dxx(u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     du = Matrix{T}(undef, size(u)...)
     dxx!(du, u, Omega)
     return du
 end
 
-function dxx!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
+function dxx!(du::M, u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     @inbounds for j in axes(du, 2)
         for i in axes(du, 1)[2:Omega.nx-1]
@@ -28,14 +75,43 @@ function dxx!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
     end
 end
 
-function dyy(u::M, Omega::ComputationDomain{T, L, M}) where
+function dxx!(du::M, u::M, Omega::RegionalComputationDomain{T, L, M},
+    method::FiniteDiffParams{T}) where {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
+    
+    (; r, d2_central, d2_forward, d2_backward) = method
+    for j in axes(u, 2)
+        for i in axes(u, 1)[r+1:end-r]
+            @inbounds for ii in eachindex(d2_central.grid)
+                du[i, j] = muladd(d2_central.coefs[ii],
+                    u[i + d2_central.grid[ii], j], du[i, j])
+            end
+        end
+        for i in axes(u, 1)[1:r]
+            @inbounds for ii in eachindex(d2_forward.grid)
+                du[i, j] = muladd(d2_forward.coefs[ii],
+                    u[i + d2_forward.grid[ii], j], du[i, j])
+            end
+        end
+        for i in axes(u, 1)[end-r+1:end]
+            @inbounds for ii in eachindex(d2_backward.grid)
+                du[i, j] = muladd(d2_backward.coefs[ii],
+                    u[i + d2_backward.grid[ii], j], du[i, j])
+            end
+        end
+    end
+    du ./= (Omega.Dx .^ 2)
+    return nothing
+end
+
+
+function dyy(u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     du = Matrix{T}(undef, size(u)...)
     dyy!(du, u, Omega)
     return du
 end
 
-function dyy!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
+function dyy!(du::M, u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     @inbounds for i in axes(du, 1)
         for j in axes(du, 2)[2:Omega.ny-1]
@@ -47,7 +123,38 @@ function dyy!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
     end
 end
 
-function dxy(u::M, Omega::ComputationDomain{T, L, M}) where
+function dyy!(du::M, u::M, Omega::RegionalComputationDomain{T, L, M},
+    method::FiniteDiffParams{T}) where {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
+    
+    (; r, d2_central, d2_forward, d2_backward) = method
+    @inbounds for i in axes(du, 1)
+        @inbounds for j in axes(du, 2)[r+1:end-r]
+            @inbounds for jj in eachindex(d2_central.grid)
+                du[i, j] = muladd(d2_central.coefs[jj],
+                    u[i, j + d2_central.grid[jj]], du[i, j])
+            end
+        end
+
+        @inbounds for j in axes(du, 2)[1:r]
+            @inbounds for jj in eachindex(d2_forward.grid)
+                du[i, j] = muladd(d2_forward.coefs[jj],
+                    u[i, j + d2_forward.grid[jj]], du[i, j])
+            end
+        end
+
+        @inbounds for j in axes(du, 2)[end-r+1:end]
+            @inbounds for jj in eachindex(d2_backward.grid)
+                du[i, j] = muladd(d2_backward.coefs[jj],
+                    u[i, j + d2_backward.grid[jj]], du[i, j])
+            end
+        end
+    end
+
+    du ./= (Omega.Dy .^ 2)
+    return nothing
+end
+
+function dxy(u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     ux = Matrix{T}(undef, size(u)...)
     uxy = Matrix{T}(undef, size(u)...)
@@ -61,7 +168,7 @@ function dxy!(ux, uxy, u, Omega)
     dy!(uxy, ux, Omega)
 end
 
-function dx!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
+function dx!(du::M, u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     @inbounds for j in axes(du, 2)
         for i in axes(du, 1)[2:Omega.nx-1]
@@ -72,7 +179,7 @@ function dx!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
     end
 end
 
-function dy!(du::M, u::M, Omega::ComputationDomain{T, L, M}) where
+function dy!(du::M, u::M, Omega::RegionalComputationDomain{T, L, M}) where
     {T<:AbstractFloat, L<:Matrix{T}, M<:Matrix{T}}
     @inbounds for i in axes(du, 1)
         for j in axes(du, 2)[2:Omega.ny-1]
