6ae513517aa7f25b44fb3f3926b8d4a5a348e878
Documenter.jl <documenter@juliadocs.github.io>
2025-07-30 17:55:55 +0000

    build based on 0834ccd

---
+++ dev/examples/benchmark_analytic.jl
@@ -0,0 +1,92 @@
+#=
+# Analytical benchmark
+
+## Explicit time stepping (recommended)
+
+In this example, we want to compute the viscous displacement of the upper mantle resulting from a cylindrical ice load with a radius of 1000 km and a thickness of 1 km. To do so, we first generate the computation domain and the load:
+=#
+
+using FastIsostasy, CairoMakie
+
+W, n = 3f6, 7   # square domain with size 2*W and 2^n points in each dimension
+domain = RegionalDomain(W, n)
+
+H_ice_0 = zeros(domain)             # Load: 0 at the beginning...
+H_ice_1 = 1f3 .* (domain.R .< 1f6)  # cylinder afterwards
+fig = plot_load(domain, H_ice_1)
+
+#=
+This looks good! `plot_load` is a function of FastIsostasy that, similar to all functions `plot_*`, is only loaded if the user is `using Makie`. These functions are here to help the user visualize quickly the results that were obtained and are summarized in the [API reference]().
+
+First, we wrap the ice load as a time interpolator so that the boundary doncitions of FastIsostasy can be updated internally (if you want to update the ice thickness externally, have a look at [this]()). Then we define a solid earth structure, a sea-level model (that is inactive by default) and an output object. The simulation is subsequently executed with `run!()`. The Earth structure deserves particular attention, since it determines key properties of the problem. We here define a single layer boundary at 88 km depth. This means that the lithosphere is 88 km thick and the mantle below it is assumed to be a Maxwell body with a viscosity of 1f21 Pa s. The lithosphere density is set to 0, such that its displacement does not contribute to the pressure term of the isostatic adjustment (this is done to match Bueler et al., 2007).
+=#
+
+t_ice = [0, 1, 50f3]
+H_ice = [H_ice_0, H_ice_1, H_ice_1]
+it = TimeInterpolatedIceThickness(t_ice, H_ice, domain)
+
+bcs = BoundaryConditions(domain, ice_thickness = it)
+
+solidearth = SolidEarth(    # Use the same geometry as in Bueler et al. (2007).
+    domain,
+    layer_boundaries = [88f3],
+    layer_viscosities = [1f21],
+    rho_litho = 0f0,
+)
+
+sealevel = SeaLevel()               # Will be inactive if not specified.
+
+nout = NativeOutput(vars = [:u],    # only store viscous displacement.
+    t = [100, 500, 1500, 5000, 10_000, 50_000f0])
+
+sim = Simulation(domain, bcs, sealevel, solidearth; nout = nout)
+run!(sim)
+fig = plot_()
+
+#=
+The viscous displacement field that we obtained is quite intuitive: its shape is largely determined by the forcing and its maximal amplitude is a fraction of the ice thickness is about \dfrac{ \rho_\mathrm{ice} }{ \rho_\mathrm{mantle} }. This simple example presents the advantage to have an analytical solution, which can be plotted along our solution:
+=#
+
+fig = plot_transect(sim, [:u], analytic_cylinder_solution = true)
+
+#=
+Our numerical solution yields very small error!
+
+FastIsostasy was designed to be computationally efficient. We are therefore particularly interested in the time needed for the computation, which is stored in the `nout` object of the simulation.
+=#
+
+println("Computation time using explicit time stepping: $(sim.nout.computation_time)")
+
+#=
+This is the (compilation + computation) time that was required to compute 50 kyr of viscous displacement with a domain of 128x128 points!
+
+## Implicit time stepping
+
+If the Earth structure is laterally constant (i.e. the lithospheric thickness and the mantle viscosity do not vary in x and y), the performance can be improved by using an implicit time stepping, as derived by Bueler et al. (2007). This can be achieved by specifying the lithosphere as `RigidLithosphere()` or as `LaterallyConstantLithosphere()` and requires to set a fixed time step via the `DiffEqOptions` in the `SolverOptions`:
+=#
+
+solidearth = SolidEarth(
+    domain,
+    lithosphere = RigidLithosphere(),
+    layer_boundaries = [88f3],
+    layer_viscosities = [1f21],
+)
+opts = SolverOptions(diffeq = DiffEqOptions(alg = Euler(), dt_min = 100f0))
+sim_implicit = Simulation(domain, bcs, sealevel, solidearth; nout = nout, opts = opts)
+run!(sim_implicit)
+fig = plot_transect(sim_implicit, [:u], analytic_cylinder_solution = true)
+
+#=
+The results appear to be comparable with the previously obtained ones! However, the computation time is lower:
+=#
+
+println("Computation time using implicit time stepping: $(sim_implicit.nout.computation_time)")
+
+#=
+!!! warning "Implicit time stepping is very specific"
+    If you are not sure whether your Earth structure is laterally constant, you should not use the implicit time stepping. The results will be wrong if the lithosphere thickness or the mantle viscosity vary in x and y.
+
+All of the computations shown above are performed with `Float32` as floating point precision. It is easy to switch to `Float64`, by passing arguments as such (e.g. `W = 3e6`). This will increase the accuracy of the results, but also the computation time. The default `Float32` is sufficient for most applications.
+
+Of course, this example remains simple. If you want to learn how to increase the complexity of your simulations, go to the next examples!
+=#
--- dev/examples/glacialcycle.jl
+++ dev/examples/glacialcycle.jl
@@ -1,100 +1,87 @@
 #=
 # Glacial cycle
 
-We now want to provide an example that presents:
-- a heterogeneous lithosphere thickness,
-- a heterogeneous upper-mantle viscosity,
+The previous examples focused on benchmarking FastIsostasy against analytical, numerical 1D and numerical 3D solutions. However, these cases were largely idealised. To test the model on more realistic simulations, we now provide compute the GIA response in Antarctica over the last glacial cycle. This includes the use of:
+- a heterogeneous lithosphere thickness [pan-influence-2022](@citep),
+- a heterogeneous upper-mantle viscosity [pan-influence-2022](@citep),
 - a stack of few viscous channels,
-- a more elaborate load that evolves over time,
+- a more elaborate load that evolves over time [peltier-comment-2018](@citep),
 - transient changes of the relative sea-level.
 
-For this we run a glacial cycle of Antarctica with lithospheric thickness and upper-mantle viscosity from [wiens-seismic-2022](@citet) and the ice thickness history from [peltier-comment-2018](@citet). We start by generating a [`ComputationDomain`](@ref) with intermediate resolution for the sake of the example and load the heterogeneous lithospheric from [pan-influence-2022](@citet) thanks to the convenience of [`load_dataset`](@ref):
+We start by generating a [`RegionalDomain`](@ref) with intermediate resolution for the sake of the example and load the ice history thanks to the convenience of [`load_dataset`](@ref). To get an idea of the ICE6G forcing, the ice thickness is visualised at the last glacial maximum (LGM):
 =#
 
 using CairoMakie, FastIsostasy
 
-N = 140     # corresponds to 50 km resolution
-Omega = ComputationDomain(3500e3, 3500e3, N, N)
-(; Lon, Lat) = Omega
-c = PhysicalConstants()
+N = 140
+domain = RegionalDomain(3500e3, 3500e3, N, N)   # resolution = 50 km
+Lon, Lat = domain.Lon, domain.Lat
+(_, _, t), Hice, Hitp = load_dataset("ICE6G_D")
+Hice_vec = [Hitp.(Lon, Lat, tk) for tk in t]
+k_lgm = argmax([mean(Hice_vec[k]) for k in eachindex(Hice_vec)])
+plot_load(domain, Hice_vec[k_lgm])
+
+#=
+This already looks like a much more exciting ice thickness field! Here again, the ice history is wrapped into an interpolator, which is passed to an instance of `BoundaryConditions`. We define the `SeaLevel` to include the gravitational response by making the surface a `LaterallyVariableSeaSurface`. Furthermore, we allow the changes in sea level to affect the deformational response of the solid Earth by setting `InteractiveSealevelLoad`. Finally, we compute the evolution of the barystatic sea level (BSL) according to a piece-wise constant approximation of the ocean surface as a function of the BSL:
+=#
+
+it = TimeInterpolatedIceThickness(t .* 1e3, Hice_vec, domain)
+bcs = BoundaryConditions(domain, ice_thickness = it)
+sealevel = SeaLevel(
+    surface = LaterallyVariableSeaSurface(),
+    load = InteractiveSealevelLoad(),
+    bsl = PiecewiseConstantBSL(),
+)
+
+#=
+Finally, we load the interpolators of earth structure thanks to the convenience function [`load_dataset`](@ref). 
+=#
 
 (_, _), Tpan, Titp = load_dataset("Lithothickness_Pan2022")
 Tlitho = Titp.(Lon, Lat) .* 1e3                     # convert from m to km
-
-function nicer_heatmap(X)
-    fig = Figure(size = (800, 700))
-    ax = Axis(fig[1, 1], aspect = DataAspect())
-    hidedecorations!(ax)
-    hm = heatmap!(ax, X)
-    Colorbar(fig[1, 2], hm, height = Relative(0.6))
-    return fig
-end
-nicer_heatmap(Tlitho)
-
-#=
-In a similar way, we can load the log-viscosity field from [pan-influence-2022](@citet) and plot it at about 300 km depth
-=#
-
 (_, _, _), _, logeta_itp = load_dataset("Viscosity_Pan2022")
-logeta300 = logeta_itp.(Lon, Lat, c.r_equator - 300e3)
-nicer_heatmap(logeta300)
 
 #=
-The number of layers and the depth of viscous half-space are arbitrary parameters that have to be defined by the user. We here use a relatively shallow model (half-space begins at 300 km depth) with 1 equalisation layer and 3 intermediate layers:
+The number of layers and the depth of viscous half-space are arbitrary parameters that have to be defined by the user. We here use a relatively shallow model (half-space begins at 300 km depth) with 1 equalisation layer and 3 intermediate layers.
 =#
 
 mindepth = maximum(Tlitho) + 1e3
-lb_vec = range(mindepth, stop = 300e3, length = 3)
-lb = cat(Tlitho, [fill(lbval, Omega.Nx, Omega.Ny) for lbval in lb_vec]..., dims=3)
-
+layerboundary_vec = range(mindepth, stop = 300e3, length = 3)
+lb = cat(Tlitho, [fill(lbval, domain.nx, domain.ny)
+    for lbval in layerboundary_vec]..., dims=3)
 rlb = c.r_equator .- lb
 nlb = size(rlb, 3)
 lv_3D = 10 .^ cat([logeta_itp.(Lon, Lat, rlb[:, :, k]) for k in 1:nlb]..., dims=3)
-size(lv_3D)
-
-#=
-To prevent extreme values of the viscosity, we require it to be larger than a minimal value, fixed to be $$10^{16} \, \mathrm{Pa \, s} $$. We subsequently generate a [`LayeredEarth`](@ref) that embeds all the information that has been loaded so far:
-=#
 
 eta_lowerbound = 1e16
 lv_3D[lv_3D .< eta_lowerbound] .= eta_lowerbound
-p = LayeredEarth(Omega, layer_boundaries = lb, layer_viscosities = lv_3D)
-nicer_heatmap(log10.(p.effective_viscosity))
+maskactive = gaussian_smooth(Hice_vec[k_lgm], domain, 0.05, 0) .> 10
+solidearth = SolidEarth(
+    domain,
+    layer_boundaries = lb,
+    layer_viscosities = lv_3D,
+    maskactive = maskactive,    # required when using `InteractiveSealevelLoad`
+)
+fig = plot_earth(domain, solidearth)
 
 #=
-We now load the ice thickness history from ICE6G, again helped by the convenience of [`load_dataset`](@ref). We then create a vector of anomaly snapshots, between which FastIsostasy automatically interpolates linearly. To get an idea of ICE6G, the ice thickness anomaly is then visualised at LGM:
+This already looks like a much more exciting solid Earth structure!
+
+Finally, we define the output struct, the [`Simulation`](@ref) and `run!` it.
 =#
 
-(lon, lat, t), Hice, Hitp = load_dataset("ICE6G_D")
-Hice_vec = [Hitp.(Lon, Lat, tk) for tk in t]
-nicer_heatmap(Hitp.(Lon, Lat, -26) - Hitp.(Lon, Lat, 0))
+nout = NativeOutput(vars = [:u, :ue, :dz_ss, :z_ss, :H_ice], t = [-26f3, -12f3, 0])
+sim = Simulation(domain, bcs, sealevel, solidearth; nout = nout)
+run!(sim)
+println("Computation time: ", sim.nout.computation_time)
 
 #=
-Finally, we define and solve the resulting [`FastIsoProblem`](@ref). We hereby choose the `verbose=true` option to track the progress of the computation.
+Ok, that was fast! Let's visualise three snapshots of displacements that roughly correspond to LGM, the end of the meltwater pulse 1A and the present-day:
 =#
 
-opts = SolverOptions(verbose = true)
-tyr = t .* 1e3
-fip = FastIsoProblem(Omega, c, p, tyr, tyr, Hice_vec, output = "sparse", opts = opts)
-solve!(fip)
+copts = (colormap = :PuOr, colorrange = (-500, 500))
+fig = plot_out_over_time(sim, :u_tot, [-26f3, -12f3, 0], copts)
 
 #=
-For a resolution of 50 km, the computation time of this last step is less than a minute on a modern i7 (Intel i7-10750H CPU @ 2.60GHz)! We visualise three snapshots of displacements that roughly correspond to LGM, the end of the meltwater pulse 1A and the present-day:
-=#
-
-tplot = [-26, -12, 0]
-fig = Figure(size = (1200, 400))
-opts = ( colormap = :PuOr, colorrange = (-400, 400) )
-for k in eachindex(tplot)
-    kfi = argmin( abs.(tplot[k] * 1e3 .- tyr) )
-    ax = Axis(fig[1, k], aspect = DataAspect(), title = "t = $(t[kfi]) kyr")
-    hidedecorations!(ax)
-    heatmap!(ax, fip.out.u[kfi] + fip.out.ue[kfi]; opts...)
-    println(kfi)
-end
-Colorbar(fig[1, 4], height = Relative(0.6); opts...)
-fig
-
-#=
-The displayed fields are displacement anomalies w.r.t. to the last interglacial, defined as the reference for the ice thickness anomalies. In [swierczek2024fastisostasy](@citet) these computations are performed on a finer grid, with an interactive sea level, and show great agreement with a 3D GIA model that runs between 10,000-100,000 slower (however at with advantage of obtaining a global and richer output).
+This looks very much like what is obtained by Seakon (Swierczek-Jereczek et al., 2024, Fig.9.g), a 3D GIA model.
 =#
